a7f8ff1feb86e8e5ebefa0e8a4d64ac4a8220b9e
tim_jackins <jack@jacktimmins.com>
2021-05-19 02:04:33 -0400

    Added setup.py for easy install

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='roll',
+    version=read('VERSION'),
+    description='A command line tool which simulates rolling a die',
+    long_description=read('README.md'),
+    long_description_content_type='text/x-markdown',
+    keywords='D&D Dice Roll CLI',
+    author='David Haller',
+    author_email='davidhaller@mailbox.org',
+    license='GPLv3',
+    url='https://github.com/davidhaller/roll',
+    download_url = 'https://github.com/davidhaller/roll/archive/master.zip',
+    packages=find_packages(),
+    package_data={
+        'roll': ['../VERSION']
+    },
+    entry_points={
+        'console_scripts': [
+            'roll = roll.__main__:main',
+        ]
+    },
+    python_requires='>=3.6',
+)
