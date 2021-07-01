52928695ec236db2ce365eacae5ad46aa5ffaa41
David Haller <davidhaller@mailbox.org>
2021-07-01 12:42:11 +0200

    Update mail address

--- roll/__main__.py
+++ roll/__main__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2021 David Haller <davidhaller@mailbox.org>
+# Copyright (C) 2024 David Haller <haller_david@icloud.com>
 #
 # roll is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
